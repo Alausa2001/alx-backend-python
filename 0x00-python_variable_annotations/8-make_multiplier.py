75b020ed395f78f2b17bf8b2df7b62f74bf2ea12
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-02 21:05:10 +0100

    changed the arg of the lambda function in the return statement from int to float

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,4 +8,4 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier"""
-    return lambda x: multiplier * int(x)
+    return lambda x: multiplier * x
