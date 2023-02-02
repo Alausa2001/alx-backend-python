45444617ac678bb9ac79c05edf095a6b8eaa0b90
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-02 18:22:03 +0100

    a annotated function that takes a string and int/float and return their tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,8 +7,6 @@ The first element of the tuple is the string k.
 The second element is the square of the int/float v
 and should be annotated as a float.
 """
-
-
 from typing import Union, Tuple
 
 
