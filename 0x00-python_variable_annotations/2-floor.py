b3ab3c276598e2cd5727c5280672eb613cd0da78
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-02 16:03:55 +0100

    corrected the module documentation

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-from math import floor as round_up
-
 """
 Write a type-annotated function floor which takes a float n as
 argument and returns the floor of the float
 """
+import math
 
 
 def floor(n: float) -> int:
     """returns the floor a float"""
-    return round_up(n)
+    return math.floor(n)
