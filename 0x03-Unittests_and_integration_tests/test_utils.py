31f2778f5ebdca3730ea39f8eb8cdfd823cedf3f
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-26 22:48:51 +0100

    created a mocked test for a http call

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,10 @@
 """
 contains tests for utils.py
 """
+import utils
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
@@ -28,3 +30,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """test for if an exception is raised"""
         with self.assertRaises(KeyError):
             access_nested_map(paramA, paramB)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    test class of get_json in utils.py
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, url, expected):
+        """
+        tests external http calls
+        """
+
+        with patch('utils.requests') as mock_requests:
+            response = Mock()
+            mock_requests.get.return_value = response
+            response.json.return_value = expected
+            self.assertEqual(get_json(url), response.json.return_value)
