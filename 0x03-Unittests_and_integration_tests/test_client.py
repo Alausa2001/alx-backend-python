464bd73145accab8c4597b085c1c3e37529d0c8e
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-27 17:40:23 +0100

    added test for client.GithubOrgClient._public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 contain tests for class/methods in client.py
 """
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, PropertyMock
 from parameterized import parameterized
 # from utils import get_json, access_nested_map, memoize
 from client import GithubOrgClient
@@ -29,3 +29,20 @@ class TestGithubOrgClient(unittest.TestCase):
         test.org()
         test.org()
         get_json_mock.assert_called_once_with(endpoint)
+
+    # @patch('GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self):
+        """
+        test _public_repos_url method in client.GithubOrgClient
+        """
+        return_dict = {
+                "url": "https://api.github.com/orgs/google",
+                "repos_url": "https://api.github.com/orgs/google/repos"
+                }
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            test = GithubOrgClient('google')
+            mock.return_value = return_dict
+            _public_repos_url = test._public_repos_url
+            repos_url = "https://api.github.com/orgs/google/repos"
+            self.assertEqual(_public_repos_url, repos_url)
